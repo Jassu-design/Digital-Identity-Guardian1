34698e88bf4d6d8be34db4a9e4c140552766798c
Jaswant Prajapat <bkmodi624@mail.com>
2026-07-16 12:57:43 +0530

    add app.js of client

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <main>
+      <h1>Digital Identity Guardian</h1>
+      <p>Protect your digital identity from suspicious messages.</p>
+    </main>
+  )
+}
+
+export default App
